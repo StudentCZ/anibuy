8c1164c3c40a650ccba4dbec59a119964f42ab47
Chao Zhou <Czhou0k@gmail.com>
2023-03-10 14:43:51 -0500

    added product table

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,24 @@
 CREATE TABLE Users (
   user_id SERIAL PRIMARY KEY,
-  username: VARCHAR(255) NOT NULL,
-  password: VARCHAR(255) NOT NULL,
-  email: VARCHAR(255) NOT NULL
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  phone_number VARCHAR(25),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  state VARCHAR (255),
+  country VARCHAR (255),
+  zip_code VARCHAR (255)
+);
+
+CREATE TABLE Product (
+  product_id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  price DECIMAL(10, 2) NOT NULL,
+  stock_quantity INT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
