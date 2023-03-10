cbd3b81178b5c6a232250fd7ee024cce942203df
Chao Zhou <Czhou0k@gmail.com>
2023-03-09 21:46:03 -0500

    added routes file and middleware for routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(5001, () => {
+  console.log('port 5001 has started');
+});
