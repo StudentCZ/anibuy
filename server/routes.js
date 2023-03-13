f18e4eb8eebdf657b284bdac762c498dc6b034da
Chao Zhou <Czhou0k@gmail.com>
2023-03-13 02:28:25 -0400

    added get request for all users

--- server/routes.js
+++ server/routes.js
@@ -2,4 +2,18 @@ const express = require('express');
 const router = express.Router();
 const db = require('./db');
 
+router.use(express.json());
+
+router.get('/users', async (req, res) => {
+  try {
+    const result = await db.query(
+      `SELECT user_id, username, email, first_name, last_name, phone_number, created_at, updated_at FROM Users`
+    );
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
 module.exports = router;
