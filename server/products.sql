0a780aa9ca2dfb74f355aa86a9c77c2f85484ea7
Chao Zhou <Czhou0k@gmail.com>
2023-03-17 15:38:59 -0400

    added another product to sql file

--- server/products.sql
+++ server/products.sql
@@ -4,5 +4,6 @@ INSERT INTO Products (
 ('Laptops', 'Brand new latest models for laptops at an afforable price', 1000, 100),
 ('Phones', 'Check out our brand new phone models that are affordable and using latest technologies', 1000, 100),
 ('Smartwatches', 'A stylish and functional smartwatch that tracks your fitness, progress, notifications and so much more', 250, 50),
-('TVs', 'Check out our latest selections of smart tvs with high definition, 4K graphics', 1000, 50)
+('TVs', 'Check out our latest selections of smart tvs with high definition, 4K graphics', 1000, 50),
+('IPads', 'Discover the latest IPads models that offer powerful performance, stunning visuals and a range of useful features to enchance your work and entertainment experience', 500, 50)
 ;
