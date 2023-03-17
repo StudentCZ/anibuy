6411b9eea13f0b0dd864af2dc6330cf5b7a8b92b
Chao Zhou <Czhou0k@gmail.com>
2023-03-17 16:29:05 -0400

    added a few more categories

--- server/categories.sql
+++ server/categories.sql
@@ -1,5 +1,8 @@
 INSERT INTO Categories (name, description)
 VALUES ("Electronics", "Check out our latest selections of electronics"),
 ("Entertainment", "A variety of selections of different kind of entertainment"),
-("Education", "Are you looking to skill up? Take a look at our education section")
+("Education", "Are you looking to skill up? Take a look at our education section"),
+("Games", "Lets us browse a great selection of curated games for your enjoyment"),
+("Media", "Interested in seeing high quality media that are hidden gems"),
+("Books", "For our users that love reading, we have a great selection of books")
 ;
