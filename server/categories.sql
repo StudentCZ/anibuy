60149d71fa565ed4f3b64b331ab0d9683eb6ea89
Chao Zhou <Czhou0k@gmail.com>
2023-03-17 16:24:52 -0400

    added categories to sql file

---
+++ server/categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO Categories (name, description)
+VALUES ("Electronics", "Check out our latest selections of electronics"),
+("Entertainment", "A variety of selections of different kind of entertainment"),
+("Education", "Are you looking to skill up? Take a look at our education section")
+;
